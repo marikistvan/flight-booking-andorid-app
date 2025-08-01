df0bae6c85e20e96b6f632f3d661781d84b6ce65
Marik Istvan <marikistvan2002@gmail.com>
2025-08-01 23:28:37 +0200

    simple refact

--- src/app/pages/flightSearch/fligthList/flightList.component.ts
+++ src/app/pages/flightSearch/fligthList/flightList.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, Signal, signal, WritableSignal } from "@angular/core";
+import { Component } from "@angular/core";
 import { ModalDialogParams } from "@nativescript/angular";
 import { Dictionaries, FlightOffer, FlightOffersResponse } from "~/app/models/flight-offers-response";
-
 
 @Component({
   selector: "ns-flight-list",
@@ -9,13 +8,14 @@ import { Dictionaries, FlightOffer, FlightOffersResponse } from "~/app/models/fl
   styleUrls: ["./flightList.component.scss"],
 })
 export class FlightListComponent {
-  mainArrivalCity:string;
+  mainArrivalCity: string;
   public flightOffers: FlightOffer[];
-  dictionary:Dictionaries;
+  dictionary: Dictionaries;
   constructor(private modalDialogParams: ModalDialogParams) {
+
     this.flightOffers = this.modalDialogParams.context.flightOffers;
-     this.mainArrivalCity=this.modalDialogParams.context.toPlace;
-     this.dictionary=this.modalDialogParams.context.dictionary;
+    this.mainArrivalCity = this.modalDialogParams.context.toPlace;
+    this.dictionary = this.modalDialogParams.context.dictionary;
   }
   onCancel() {
     this.modalDialogParams.closeCallback(null);
