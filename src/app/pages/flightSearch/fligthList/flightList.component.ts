a6766cd6e467fac91daad00e3e269c0e9a309e45
Marik Istvan <marikistvan2002@gmail.com>
2025-08-27 23:01:08 +0200

    standalone, routerextension

--- src/app/pages/flightSearch/fligthList/flightList.component.ts
+++ src/app/pages/flightSearch/fligthList/flightList.component.ts
@@ -1,22 +1,29 @@
-import { Component } from "@angular/core";
-import { ModalDialogParams } from "@nativescript/angular";
-import { Dictionaries, FlightOffer, FlightOffersResponse } from "~/app/models/flight-offers-response";
+import { CommonModule } from "@angular/common";
+import { Component, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule, RouterExtensions } from "@nativescript/angular";
+import { FlightSearchStateService } from "~/app/services/flight-search-state.service";
+import { FlightListRowComponent } from "./flightList-row/flightList-row.component";
 
 @Component({
+  standalone: true,
   selector: "ns-flight-list",
   templateUrl: "./flightList.component.html",
-  styleUrls: ["./flightList.component.scss"],
+  styleUrls: ["./flightList.component.scss",],
+  imports: [
+    CommonModule,
+    NativeScriptCommonModule,
+    FlightListRowComponent
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
+
 })
-export class FlightListComponent {
-  mainArrivalCity: string;
-  public flightOffers: FlightOffer[];
-  dictionary: Dictionaries;
-  constructor(private modalDialogParams: ModalDialogParams) {
-    this.flightOffers = this.modalDialogParams.context.flightOffers;
-    this.mainArrivalCity = this.modalDialogParams.context.toPlace;
-    this.dictionary = this.modalDialogParams.context.dictionary;
-  }
+export class FlightListComponent{
+  constructor(
+    private routerExtensions: RouterExtensions,
+    public searchService: FlightSearchStateService
+  ) {}
+  
   onCancel() {
-    this.modalDialogParams.closeCallback(null);
+    this.routerExtensions.back();
   }
 }
