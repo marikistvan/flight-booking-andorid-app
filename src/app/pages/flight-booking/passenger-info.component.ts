0e7fb1b67660639b3d5660e00e200296c9e03ed4
Marik Istvan <marikistvan2002@gmail.com>
2025-08-27 22:45:38 +0200

    make it standalone and has errorHangling

--- src/app/pages/flight-booking/passenger-info.component.ts
+++ src/app/pages/flight-booking/passenger-info.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit, ViewContainerRef } from "@angular/core";
-import { ModalDialogOptions, ModalDialogService } from "@nativescript/angular";
+import { Component, Input, NO_ERRORS_SCHEMA, OnInit, ViewContainerRef } from "@angular/core";
+import { ModalDialogOptions, ModalDialogService, NativeScriptCommonModule, RouterExtensions } from "@nativescript/angular";
 import { FormGroup } from "@angular/forms";
 import { Dictionaries, FlightOffer } from "~/app/models/flight-offers-response";
 import { SetpassengerComponent } from './set-passenger/set-passenger.component';
 import { Passenger } from "~/app/models/passenger";
 import { AmadeusService } from "~/app/services/amadeus.service";
-import seatmap from '~/assets/seatmap.json';
-import { FlightSummaryComponent } from "./flight-summary/flight-summary.component";
-import { Seatmap } from "~/app/models/seatmap-response";
-
+import { firstValueFrom } from "rxjs";
+import { CommonModule } from "@angular/common";
+import { FlightSearchStateService } from "~/app/services/flight-search-state.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "ns-passenger-info",
+  standalone: true,
   templateUrl: "./passenger-info.component.html",
   styleUrls: ["./passenger-info.component.scss"],
+  imports: [
+    CommonModule,
+    NativeScriptCommonModule,
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 
 export class PassengerInfoComponent implements OnInit {
+  @Input() flightId: string;
   dictionary: Dictionaries;
   flightOffer: FlightOffer;
   passengersNumber: number;
@@ -27,21 +34,23 @@ export class PassengerInfoComponent implements OnInit {
 
   controlIndex = 0;
 
+  constructor(
+    private modalDialogSerivce: ModalDialogService,
+    private viewContainerRef: ViewContainerRef,
+    private amadeusService: AmadeusService,
+    private searchStateService: FlightSearchStateService,
+    private routerExtensions: RouterExtensions,
+     private route: ActivatedRoute
+  ) { }
+
   ngOnInit(): void {
+    this.flightId = this.route.snapshot.paramMap.get('flightId')!;
+    this.dictionary = this.searchStateService.getDictionary();
+    this.flightOffer = this.searchStateService.getFlightById(this.flightId);
+    this.passengersNumber = this.flightOffer.travelerPricings.length;
     this.genRows();
     this.setPassengerType();
     this.setPassengersArray();
-  }
-  constructor(/*private modalDialogParams: ModalDialogParams,*/
-    private modalDialogSerivce: ModalDialogService,
-    private viewContainerRef: ViewContainerRef,
-    private amadeusService: AmadeusService
-  ) {
-    // this.dictionary = modalDialogParams.context.dictionary;
-    // this.flightOffer = modalDialogParams.context.flight;
-    this.dictionary = this.amadeusService.getMockFlightOffers().dictionaries;
-    this.flightOffer = this.amadeusService.getMockFlightOffers().data[1];
-    this.passengersNumber = this.flightOffer.travelerPricings.length;
   }
 
   setPassengerInfo() { }
@@ -54,22 +63,12 @@ export class PassengerInfoComponent implements OnInit {
   }
 
   onCancel() {
-    //  this.modalDialogParams.closeCallback(null);
+    this.routerExtensions.back();
   }
   async submit() {
     if (this.isAllDataIsFilledIn()) {
-      const option: ModalDialogOptions = {
-        context: {
-          flightOffer: this.flightOffer,
-          dictionaries: this.dictionary,
-          passengers: this.passengers,
-        },
-        fullscreen: true,
-        viewContainerRef: this.viewContainerRef
-      }
-      await this.modalDialogSerivce.showModal(FlightSummaryComponent, option).then((result) => {
-
-      });
+      this.searchStateService.setPassengers(this.passengers);
+      this.routerExtensions.navigate(['flightSummary', this.flightOffer.id]);
     }
   }
 
@@ -86,34 +85,63 @@ export class PassengerInfoComponent implements OnInit {
     });
   }
   async setPassenger(id: string) {
-    const title = id + '. utas (' + this.passengersType[Number(id) - 1] + ')';
+    const title = `${id}. utas (${this.passengersType[Number(id) - 1]})`;
     const passenger: Passenger = this.passengers[Number(id) - 1];
-    let arrivalSeatCount = undefined;
+    let arrivalSeatCount: number | undefined = undefined;
+
     if (this.flightOffer.itineraries.length > 1) {
       arrivalSeatCount = this.flightOffer.itineraries[1].segments.length;
     }
-    // const seatMapDatas = (await firstValueFrom(this.amadeusService.getSeatMap(this.flightOffer))).data;
-    const seatMapDatas:Seatmap[]=seatmap.data;
-    const option: ModalDialogOptions = {
-      context: {
-        title: title,
-        passenger: passenger,
-        flightOffer: this.flightOffer,
-        seatMap: seatMapDatas
-      },
-      fullscreen: true,
-      viewContainerRef: this.viewContainerRef
-    }
-    await this.modalDialogSerivce.showModal(SetpassengerComponent, option).then((result) => {
-      if (result) {
-        const [passenger, action] = result;
-        if (action !== 'cancel') {
-          this.passengers[Number(id) - 1] = passenger;
-          this.nextPassenger(Number(id) - 1) === 0 ? '' : this.setPassenger(this.nextPassenger(Number(id) - 1).toString());
+
+    try {
+      const seatMapResponse = await firstValueFrom(
+        this.amadeusService.getSeatMap(this.flightOffer)
+      );
+
+      const seatMapDatas = seatMapResponse.data;
+
+      const option: ModalDialogOptions = {
+        context: {
+          id,
+          title,
+          passenger,
+          flightOffer: this.flightOffer,
+          seatMap: seatMapDatas
+        },
+        fullscreen: true,
+        viewContainerRef: this.viewContainerRef
+      };
+      await this.modalDialogSerivce.showModal(SetpassengerComponent, option).then((result) => {
+        if (result) {
+          const [updatedPassenger, action] = result;
+          if (action !== 'cancel') {
+            this.passengers[Number(id) - 1] = updatedPassenger;
+
+
+            const next = this.nextPassenger(Number(id) - 1);
+            if (next !== 0) {
+              this.setPassenger(next.toString());
+            }
+          }
         }
+      });
+
+    } catch (err: any) {
+      console.error("SeatMap API hiba:", err);
+
+      let userMessage = "Ismeretlen hiba történt. Kérjük, próbálja újra később.";
+      if (err?.error?.code) {
+        userMessage = this.amadeusService.handleApiError(err);
+      } else if (err.status === 404) {
+        userMessage = "A járat ülésrendje nem található.";
+      } else if (err.status === 500) {
+        userMessage = "Szerverhiba történt. Kérjük, próbálja újra később.";
       }
-    });
+
+      alert(userMessage);
+    }
   }
+
 
   nextPassenger(previousId: number): number {
     if (this.passengers.length - 1 === previousId) {
@@ -131,7 +159,7 @@ export class PassengerInfoComponent implements OnInit {
     }
     return true;
   }
-  
+
   isInvalid(controlName: string): boolean {
     const control = this.passengerForm.get(controlName);
     return control && control.invalid && (control.dirty || control.touched);
@@ -140,13 +168,13 @@ export class PassengerInfoComponent implements OnInit {
   setPassengersArray() {
     for (let i = 0; i < this.passengersNumber; i++) {
       const passenger: Passenger = {
+        id: '',
         firstName: '',
         lastName: '',
         born: '',
         sex: '',
         baggageType: '',
-        seatNumberWayThere: [],
-        seatNumberWayBack:[]
+        seats: []
       }
       this.passengers.push(passenger);
     }
