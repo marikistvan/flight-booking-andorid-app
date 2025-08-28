2c3caea78a19355ef8eef12b0bfbee769a3f1989
Marik Istvan <marikistvan2002@gmail.com>
2025-08-28 22:03:08 +0200

    delete function working

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,33 +1,55 @@
 import { Component, OnInit } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application } from '@nativescript/core'
+import { Application, Dialogs } from '@nativescript/core'
 import { RouterExtensions } from "@nativescript/angular";
 import { AuthService } from '~/app/services/auth.service';
+import { UserService } from '~/app/services/user.service';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
+  providers: [DatePipe],
   selector: 'ns-profile',
   templateUrl: './profile.component.html',
   styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private routerExtensions: RouterExtensions, public authService:AuthService) {
+  constructor(
+    private routerExtensions: RouterExtensions,
+    public authService: AuthService,
+    private userService: UserService,
+    public datePipe: DatePipe
+  ) {
   }
 
   async ngOnInit(): Promise<void> {
   }
-  userDataModify():boolean{
+  userDataModify(): boolean {
     return true;
   }
-  editPassword(){
+  editPassword() {
+
+  }
+  deleteProfile() {
+    Dialogs.confirm({
+      title: 'Confirm!',
+      message: 'Biztosan törölni akarod a fiókod?',
+      okButtonText: 'Igen',
+      cancelButtonText: 'Nem',
+      neutralButtonText: 'Mégsem',
+    }).then((result) => {
+      if (result) {
+        this.userService.deleteUser();
+      }
+    })
 
   }
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
     sideDrawer.showDrawer()
   }
-  SetProfil(): void{
+  SetProfil(): void {
     console.log("set");
   }
 }
