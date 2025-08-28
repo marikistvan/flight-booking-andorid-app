e5af2faf12702aaf778603b34c17cc05dfbb4088
Marik Istvan <marikistvan2002@gmail.com>
2025-08-28 21:58:43 +0200

    email and password validators

---
+++ src/app/validators/email-regex.validator.ts
@@ -0,0 +1,12 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function emailRegexValidator(control: AbstractControl): ValidationErrors | null {
+    const email = control.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email) {
+        return null;
+    }
+
+    return emailRegex.test(email.trim()) ? null : { emailRegex: true };
+}
