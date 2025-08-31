e27de4209685a5a10b74a5adc46f2e9a5d6b3daa
Marik Istvan <marikistvan2002@gmail.com>
2025-08-31 21:31:02 +0200

    dialog title change to Fiók törlése and delete ngOninit method

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -6,34 +6,29 @@ import { AuthService } from '~/app/services/auth.service';
 import { UserService } from '~/app/services/user.service';
 import { DatePipe } from '@angular/common';
 
-
 @Component({
   providers: [DatePipe],
   selector: 'ns-profile',
   templateUrl: './profile.component.html',
   styleUrls: ["./profile.component.scss"],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
   constructor(
     private routerExtensions: RouterExtensions,
     public authService: AuthService,
     private userService: UserService,
     public datePipe: DatePipe
-  ) {
-  }
+  ) {}
 
-  async ngOnInit(): Promise<void> {
-  }
   userDataModify(): boolean {
     return true;
   }
-  editPassword() {
+  editPassword() {}
 
-  }
   deleteProfile() {
     Dialogs.confirm({
-      title: 'Confirm!',
+      title: 'Fiók törlése!',
       message: 'Biztosan törölni akarod a fiókod?',
       okButtonText: 'Igen',
       cancelButtonText: 'Nem',
