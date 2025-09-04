d177c9813f151f448c46e4131ea28d7ebc774bba
Marik Istvan <marikistvan2002@gmail.com>
2025-09-04 22:49:46 +0200

    feat(settings): implement localization support and language selection functionality

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, signal } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application } from '@nativescript/core'
+import { action, Application, Dialogs, Utils } from '@nativescript/core'
 import { AuthService } from '~/app/services/auth.service';
+import { overrideLocale } from '@nativescript/localize';
+import { localize } from "@nativescript/localize";
 
 @Component({
 
@@ -10,10 +12,17 @@ import { AuthService } from '~/app/services/auth.service';
   styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent implements OnInit {
+  languages=signal(['Hungary','English']);
+  languageDict: Record<string, string> = {
+    'Hungary':'hu',
+    'English':'en'
+};
   constructor(public authService: AuthService) {
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    
+  }
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
@@ -21,6 +30,35 @@ export class SettingsComponent implements OnInit {
   }
   signOut(): void {
     this.authService.signOut();
+  }
 
+  setAppLanguage() {
+    action({
+      message: localize('settings.chooseLanguage'),
+      cancelButtonText: localize('general.cancel'),
+      actions: this.languages()
+    }).then(result => {
+      if (result !== localize('general.cancel')) {
+        const localeOverriddenSuccessfully = overrideLocale(this.languageDict[result]);
+        this.askUserToRestartTheApp(this.languageDict[result]);
+      }
+    });
+
+  }
+
+  async askUserToRestartTheApp(language: string) {
+    Dialogs.confirm({
+      title: localize("settings.changeLanguageTitle"),
+      message: localize("settings.changeLanguageMessage"),
+      okButtonText: localize("settings.close"),
+      neutralButtonText: localize("general.cancel"),
+    }).then((result) => {
+      if (result === true) {
+        overrideLocale(language);
+        console.log(result)
+        Utils.android.getCurrentActivity().finish();
+      }
+
+    });
   }
 }
