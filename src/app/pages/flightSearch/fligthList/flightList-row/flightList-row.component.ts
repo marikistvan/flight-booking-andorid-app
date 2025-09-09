5020e8c3784d43c7b84c10c2b8c263dc7c000444
Marik Istvan <marikistvan2002@gmail.com>
2025-09-09 17:14:50 +0200

    feat(flightList): implement localization for transfer labels and flight price summary

--- src/app/pages/flightSearch/fligthList/flightList-row/flightList-row.component.ts
+++ src/app/pages/flightSearch/fligthList/flightList-row/flightList-row.component.ts
@@ -2,6 +2,8 @@ import { CommonModule, DatePipe } from "@angular/common";
 import { Component, Input, NO_ERRORS_SCHEMA, OnInit, signal } from "@angular/core";
 import { NativeScriptCommonModule, RouterExtensions } from "@nativescript/angular";
 import { Dictionaries, FlightOffer } from "~/app/models/flight-offers-response";
+import { NativeScriptLocalizeModule } from '@nativescript/localize/angular';
+import { localize } from "@nativescript/localize";
 
 @Component({
   providers: [DatePipe],
@@ -12,6 +14,7 @@ import { Dictionaries, FlightOffer } from "~/app/models/flight-offers-response";
   imports: [
     CommonModule,
     NativeScriptCommonModule,
+    NativeScriptLocalizeModule
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
@@ -19,6 +22,8 @@ export class FlightListRowComponent implements OnInit {
   @Input({ required: true }) flight!: FlightOffer;
   @Input({ required: true }) dictionary!: Dictionaries;
   halfPrice: number;
+  summary = signal('');
+  stapOver = signal('');
   isSearchStarted = signal(false);
   constructor(
     public datePipe: DatePipe,
@@ -26,14 +31,16 @@ export class FlightListRowComponent implements OnInit {
   ) { }
   ngOnInit(): void {
     this.halfPrice = Number(this.flight.price.total) / 2;
+    this.summary.set(localize('general.summary'));
+    this.stapOver.set(localize('flightListRow.stepOver'));
   }
 
   formatDuration(duration: string): string {
     const regex = /PT(?:(\d+)H)?(?:(\d+)M)?/;
     const match = duration.match(regex);
     if (!match) return duration;
-    const hours = match[1] ? `${+match[1]} óra` : '';
-    const minutes = match[2] ? `${+match[2]} perc` : '';
+    const hours = match[1] ? `${+match[1]} ${localize('general.hour')}` : '';
+    const minutes = match[2] ? `${+match[2]} ${localize('general.minute')}` : '';
     return [hours, minutes].filter(Boolean).join(' ');
   }
   async select() {
