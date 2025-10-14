6943fee308a58bbeab892b65cd0cf736fb948e08
Marik Istvan <marikistvan2002@gmail.com>
2025-10-14 15:04:32 +0200

    refactor: simplify ActionBar layout with added section header componenet

--- src/app/pages/basket/basket.component.ts
+++ src/app/pages/basket/basket.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application,ObservableArray } from '@nativescript/core'
+import { Application, ObservableArray } from '@nativescript/core'
 import { FirebaseAuth } from '@nativescript/firebase-auth';
 import { firebase } from "@nativescript/firebase-core";
 import { localize } from '@nativescript/localize';
@@ -8,12 +7,12 @@ import { localize } from '@nativescript/localize';
 interface BasicFlightInformation {
   data_itineraries_segments_departure_at: string
   data_itineraries_segments_arrival_at: string
-  data_itineraries_segments_departure_iataCode:string
-  data_itineraries_segments_arrival_iataCode:string
-  data_itineraries_price_total:string
-  data_itineraries_duration:string
-  data_id:string
- }
+  data_itineraries_segments_departure_iataCode: string
+  data_itineraries_segments_arrival_iataCode: string
+  data_itineraries_price_total: string
+  data_itineraries_duration: string
+  data_id: string
+}
 
 @Component({
   selector: 'ns-basket',
@@ -21,77 +20,74 @@ interface BasicFlightInformation {
   styleUrls: ["./basket.component.scss"],
 })
 export class BasketComponent implements OnInit {
-  start=localize('blogItem.start');
-  arrive=localize('blogItem.arrive');
-  startIataCode=localize('blogItem.startItataCode');
-  arriveIataCode=localize('blogItem.arriveIataCode');
-  priceTotal=localize('blogItem.priceTotal');
-  duration=localize('blogItem.duration');
-  user=firebase().auth().currentUser;
+  start = localize('blogItem.start');
+  arrive = localize('blogItem.arrive');
+  startIataCode = localize('blogItem.startItataCode');
+  arriveIataCode = localize('blogItem.arriveIataCode');
+  priceTotal = localize('blogItem.priceTotal');
+  duration = localize('blogItem.duration');
+  user = firebase().auth().currentUser;
   auth = firebase().auth();
-  basicFlightInformations:ObservableArray<BasicFlightInformation>;
+  basicFlightInformations: ObservableArray<BasicFlightInformation>;
   constructor() {
   }
-  getFlightsFromDatabase():void{
-    this.basicFlightInformations= new ObservableArray<BasicFlightInformation>();
+  getFlightsFromDatabase(): void {
+    this.basicFlightInformations = new ObservableArray<BasicFlightInformation>();
     if (this.user != null) {
       firebase()
-      .firestore()
-      .collection("userBasket")
-      .doc(this.user.uid)
-      .collection("flights")
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const data=doc.data()
-          this.basicFlightInformations.push({
-            data_itineraries_segments_departure_at: data.departureAt,
-            data_itineraries_segments_arrival_at: data.arrivalAt,
-            data_itineraries_segments_departure_iataCode: data.departureIata,
-            data_itineraries_segments_arrival_iataCode:data.arrivalIata,
-            data_itineraries_price_total:data.totalPrice,
-            data_itineraries_duration:data.duration,
-            data_id:data.id,
+        .firestore()
+        .collection("userBasket")
+        .doc(this.user.uid)
+        .collection("flights")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const data = doc.data()
+            this.basicFlightInformations.push({
+              data_itineraries_segments_departure_at: data.departureAt,
+              data_itineraries_segments_arrival_at: data.arrivalAt,
+              data_itineraries_segments_departure_iataCode: data.departureIata,
+              data_itineraries_segments_arrival_iataCode: data.arrivalIata,
+              data_itineraries_price_total: data.totalPrice,
+              data_itineraries_duration: data.duration,
+              data_id: data.id,
+            });
           });
+          for (let i = 0; i < this.basicFlightInformations.length; i++) {
+            console.log(this.basicFlightInformations.getItem(i));
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching flights:", error);
         });
-    for(let i=0;i< this.basicFlightInformations.length;i++){
-      console.log(this.basicFlightInformations.getItem(i));
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching flights:", error);
-  });
     }
   }
 
   ngOnInit(): void {
     this.getFlightsFromDatabase();
-    }
-  onDrawerButtonTap(): void {
-    const sideDrawer = <RadSideDrawer>Application.getRootView()
-    sideDrawer.showDrawer()
   }
-  Delete(id:string):void{
+
+  Delete(id: string): void {
     console.log(id);
-    let index="0";
-    for(let i=0;i<this.basicFlightInformations.length;i++){
-      if(this.basicFlightInformations.getItem(i).data_id==id){
-        index=i+"";
+    let index = "0";
+    for (let i = 0; i < this.basicFlightInformations.length; i++) {
+      if (this.basicFlightInformations.getItem(i).data_id == id) {
+        index = i + "";
       }
     }
     firebase()
-    .firestore()
-    .collection("userBasket")
-    .doc(this.user.uid)
-    .collection("flights")
-    .doc(this.basicFlightInformations.getItem(parseInt(index)).data_id+'_'+this.basicFlightInformations.getItem(parseInt(index)).data_itineraries_duration)
-    .delete()
-    .then(() => {
-      console.log("Document has been successfully deleted.");
-      this.getFlightsFromDatabase();
-    })
-    .catch((error) => {
-      console.error("An error occurred during deletion: ", error);
-    });  
+      .firestore()
+      .collection("userBasket")
+      .doc(this.user.uid)
+      .collection("flights")
+      .doc(this.basicFlightInformations.getItem(parseInt(index)).data_id + '_' + this.basicFlightInformations.getItem(parseInt(index)).data_itineraries_duration)
+      .delete()
+      .then(() => {
+        console.log("Document has been successfully deleted.");
+        this.getFlightsFromDatabase();
+      })
+      .catch((error) => {
+        console.error("An error occurred during deletion: ", error);
+      });
   }
 }
