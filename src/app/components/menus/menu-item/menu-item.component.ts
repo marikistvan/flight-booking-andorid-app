fabbe7e58fc44ebcd9bf4386fbb4876d350944a0
Marik Istvan <marikistvan2002@gmail.com>
2025-10-14 18:55:43 +0200

    refact: make menu-item component and using it in the menus component

---
+++ src/app/components/menus/menu-item/menu-item.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule, RouterExtensions } from '@nativescript/angular';
+import { Application } from '@nativescript/core';
+import { NativeScriptLocalizeModule } from '@nativescript/localize/angular';
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+
+@Component({
+    selector: 'ns-menu-item',
+    standalone: true,
+    templateUrl: 'menu-item.component.html',
+    styleUrls: ["menu-item.component.scss"],
+    imports: [
+        CommonModule,
+        NativeScriptCommonModule,
+        NativeScriptLocalizeModule
+    ],
+    schemas: [NO_ERRORS_SCHEMA]
+})
+
+export class MenuItemComponent {
+    @Input({ required: true }) navlink!: string;
+    @Input({ required: true }) title!: string;
+    @Input({ required: true }) icon!: string;
+    constructor(private routerExtensions: RouterExtensions) { }
+
+    isComponentSelected(): boolean {
+        return this.routerExtensions.router.url === this.navlink;
+    }
+
+    onNavItemTap(): void {
+        this.routerExtensions.navigate([this.navlink], {
+            transition: {
+                name: 'fade',
+            },
+        })
+        const sideDrawer = <RadSideDrawer>Application.getRootView()
+        sideDrawer.closeDrawer()
+    }
+}
--- src/app/components/menus/menus.component.ts
+++ src/app/components/menus/menus.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterExtensions } from '@nativescript/angular';
-import { Application } from '@nativescript/core';
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -12,19 +9,5 @@ import { AuthService } from '../../services/auth.service';
 })
 
 export class MenusComponent {
-  constructor(private routerExtensions: RouterExtensions, public authService: AuthService) { }
-
-  isComponentSelected(url: string): boolean {
-    return this.routerExtensions.router.url === url;
-  }
-
-  onNavItemTap(navItemRoute: string): void {
-    this.routerExtensions.navigate([navItemRoute], {
-      transition: {
-        name: 'fade',
-      },
-    })
-    const sideDrawer = <RadSideDrawer>Application.getRootView()
-    sideDrawer.closeDrawer()
-  }
+  constructor(public authService: AuthService) { }
 }
