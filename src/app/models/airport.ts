062946337fdcfb09107359ed99480ca56cdcd13e
Marik Istvan <marikistvan2002@gmail.com>
2025-10-28 23:20:18 +0100

    refact: these files no longer need to be here.

--- src/app/models/airport.ts
+++
@@ -1,7 +0,0 @@
-export interface Airport {
-    data_name: string;
-    data_iataCode: string;
-    merge_nameWithIataCode: string;
-    data_address_cityName: string;
-    data_address_countryName: string;
-}
--- src/app/models/airportData.ts
+++
@@ -1,7 +0,0 @@
-type AirportData = {
-    [iataCode: string]: {
-        city: string;
-        airport: string;
-        country: string;
-    };
-};
